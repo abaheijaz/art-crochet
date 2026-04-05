932da3ebbf6b91f824990ace3f7577fb55140946
aizat <a.aizat.ismail@gmaill.com>
2026-04-05 22:43:18 +0800

    ADDED DASHBOARD PLACEHOLDER

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MainToolbar } from './core/main-toolbar/main-toolbar';
+import { Dashboard } from './features/dashboard/components/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [MainToolbar],
+  imports: [MainToolbar, Dashboard],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
