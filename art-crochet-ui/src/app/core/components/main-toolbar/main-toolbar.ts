9a46fd443db0875064156fdf28ccc26c33411d70
aizat <a.aizat.ismail@gmaill.com>
2026-04-14 20:42:09 +0800

    Change toggle to drop down

--- art-crochet-ui/src/app/core/components/main-toolbar/main-toolbar.ts
+++ art-crochet-ui/src/app/core/components/main-toolbar/main-toolbar.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-toolbar',
-  imports: [MatToolbarModule, RouterLink],
+  imports: [MatToolbarModule],
   templateUrl: './main-toolbar.html',
   styleUrl: './main-toolbar.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
