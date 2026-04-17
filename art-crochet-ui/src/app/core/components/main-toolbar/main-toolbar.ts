31fa44ea10c10edaf0e02d5da7bbf0a552391da5
aizat <a.aizat.ismail@gmaill.com>
2026-04-18 01:07:36 +0800

    updates

--- art-crochet-ui/src/app/core/components/main-toolbar/main-toolbar.ts
+++ art-crochet-ui/src/app/core/components/main-toolbar/main-toolbar.ts
@@ -1,13 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-main-toolbar',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconButton, MatIconModule, MatTooltipModule],
   templateUrl: './main-toolbar.html',
   styleUrl: './main-toolbar.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainToolbar {
   title = 'Lovingly Made';
+
+  protected readonly themeService = inject(ThemeService);
 }
