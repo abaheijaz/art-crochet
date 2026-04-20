865ef3273cc1eb5b909a53b35d34e8e0efea430d
aizat <a.aizat.ismail@gmaill.com>
2026-04-20 22:35:14 +0800

    Allow number in product name

--- art-crochet-api/src/endpoints/instagram-pictures.ts
+++ art-crochet-api/src/endpoints/instagram-pictures.ts
@@ -6,7 +6,9 @@ type AppContext = Context<{ Bindings: Env }>;
 
 function toProductTypeSlug(value: string): string {
   return value
-    .replace(/([a-z0-9])([A-Z])/g, "$1 $2")
+    .replace(/([a-z])([A-Z])/g, "$1 $2")
+    .replace(/([0-9])([A-Z])/g, "$1 $2")
+    .replace(/([a-z])([0-9])/g, "$1 $2")
     .replace(/([A-Z])([A-Z][a-z])/g, "$1 $2")
     .replace(/[^a-zA-Z0-9]+/g, " ")
     .trim()
@@ -16,7 +18,9 @@ function toProductTypeSlug(value: string): string {
 
 function toVariantLabel(value: string): string {
   return value
-    .replace(/([a-z0-9])([A-Z])/g, "$1 $2")
+    .replace(/([a-z])([A-Z])/g, "$1 $2")
+    .replace(/([0-9])([A-Z])/g, "$1 $2")
+    .replace(/([a-z])([0-9])/g, "$1 $2")
     .replace(/([A-Z])([A-Z][a-z])/g, "$1 $2")
     .replace(/[^a-zA-Z0-9]+/g, " ")
     .trim()
